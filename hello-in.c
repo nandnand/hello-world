9c6d5669eaf05fdf022ad7fbb833c546ea28c7fb
nandnand <nandnand@users.noreply.github.com>
2017-04-05 14:44:18 +0300

    created the file

---
+++ hello-in.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// Define entry point as a function named main
+
+int main(int argc, char **argv){ // given parameters for the cli
+  puts("Hello world!\n"); // log console "Hello World!"
+  return 0; // exit with 0 
+}
